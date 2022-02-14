f006c401864e382224ca265e3dc35ca8e5cac070
JunhwanLeeB <junhwan.lee_b@kurlycorp.com>
2022-02-14 23:29:26 +0900

    init project

---
+++ src/test/java/com/hexagonal/demo/HexagonalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
